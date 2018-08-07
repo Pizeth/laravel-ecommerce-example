7abf5c095c1faa9efe5570c1da164b8dd7ebe433
Razeth <seth.razeth@gmail.com>
2018-08-08 00:35:28 +0700

    front page testing

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+        /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$products = Product::where('featured', true)->take(8)->inRandomOrder()->get();
+
+        return view('test');
+    }
+}
